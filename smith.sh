8e52e50a681d43cc0aedf51400408aad1fbf69f7
Aristotel Neamtu <aneamtu@pitechnologies.ro>
2017-12-08 19:03:07 +0200

    [CINF-24] - cli script part

--- smith.sh
+++ smith.sh
@@ -6,6 +6,8 @@ GOOGLE_COMPUTE_ZONE_VAR="";
 GOOGLE_CLUSTER_NAME_VAR="";
 PROJECT_NAME_VAR="";
 CIRCLE_SHA1_VAR="";
+REPORT_URL_VAR="";
+REPORT_URL_CHANNEL_VAR="";
 SHOW_VAR="";
 
 
@@ -15,14 +17,17 @@ GOOGLE_COMPUTE_ZONE_ARG="";
 GOOGLE_CLUSTER_NAME_ARG="";
 PROJECT_NAME_ARG="";
 CIRCLE_SHA1_ARG="";
+REPORT_URL_ARG="";
+REPORT_URL_CHANNEL_ARG="";
 
 DO_ACTION="";
+DEPLOY_STATUS="";
 
 
 function usage()
 {
   echo "FORMAT: $0  ACTION [ PARAMETERS ]"
-  echo "ACTIONS: login, build-docker, push-docker, deploy, rollout-status, rollback"
+  echo "ACTIONS: login, build-docker, push-docker, deploy, rollout-status, report-status, rollback"
   echo "parameters override env vars"
   echo "PARAMETERS:"
   echo "--google-account-json=json_string"
@@ -31,6 +36,8 @@ function usage()
   echo "--google-cluster-name=cluster_name"
   echo "--project-name=project_name(app name)"
   echo "--commit-hash=full commit sha hash"
+  echo "--report-url=web_destination"
+  echo "--report-channel=channel"
   echo "--show-var  shows the variables if set"
 }
 
@@ -43,6 +50,8 @@ function read_env_vars()
   GOOGLE_CLUSTER_NAME_VAR=$GOOGLE_CLUSTER_NAME;
   PROJECT_NAME_VAR=$PROJECT_NAME;
   CIRCLE_SHA1_VAR=$CIRCLE_SHA1;
+  REPORT_URL_VAR=$REPORT_URL;
+  REPORT_URL_CHANNEL_VAR=$REPORT_URL_CHANNEL;
 }
 
 
@@ -76,6 +85,12 @@ function read_args()
       --commit-hash)
         CIRCLE_SHA1_ARG=$VALUE
         ;;
+      --report-url)
+        REPORT_URL_ARG=$VALUE
+        ;;
+      --report-channel)
+        REPORT_URL_CHANNEL_ARG=$VALUE
+        ;;
       --show-var)
         SHOW_VAR="yes"
         ;;
@@ -94,6 +109,9 @@ function read_args()
       rollout-status)
         DO_ACTION="rollout-status"
         ;;
+      report-status)
+          DO_ACTION="report-status"
+          ;;
       rollback)
         DO_ACTION="rollback"
         ;;
@@ -116,6 +134,8 @@ function override_env_vars()
   if [ ! -z "$GOOGLE_CLUSTER_NAME_ARG" ]; then GOOGLE_CLUSTER_NAME_VAR=$GOOGLE_CLUSTER_NAME_ARG; fi;
   if [ ! -z "$PROJECT_NAME_ARG" ]; then PROJECT_NAME_VAR=$PROJECT_NAME_ARG; fi;
   if [ ! -z "$CIRCLE_SHA1_ARG" ]; then CIRCLE_SHA1_VAR=$CIRCLE_SHA1_ARG; fi;
+  if [ ! -z "$REPORT_URL_ARG" ]; then REPORT_URL_VAR=$REPORT_URL_ARG; fi;
+  if [ ! -z "$REPORT_URL_CHANNEL_ARG" ]; then REPORT_URL_CHANNEL_VAR=$REPORT_URL_CHANNEL_ARG; fi;
 
 }
 
@@ -149,6 +169,9 @@ function process_action()
       ;;
     rollout-status)
       rollout_status;
+      ;;
+   report-status)
+      report_status;
       ;;
     rollback)
       rollback;
@@ -264,6 +287,30 @@ function show-var()
 
   fi;
 }
+
+
+function report_status()
+{
+  if [ -n "${PROJECT_NAME_VAR}" -a -n "${REPORT_URL_VAR}" -a -n "${CIRCLE_SHA1_VAR}", -a -n "${REPORT_URL_CHANNEL_VAR}" ];
+  then
+    STATUS_TEXT=$(kubectl rollout status deployment ${PROJECT_NAME_VAR});
+    REPORT_COLOR="danger"; #default
+    REPORT_FILTER="success";
+    DEPLOY_STATUS=$(echo $STATUS_TEXT | sed 's/"/*/g' | sed "s/'/*/g" );
+
+    if [ "$DEPLOY_STATUS" != "${DEPLOY_STATUS%$REPORT_FILTER*}" ]; then
+        REPORT_COLOR="good";
+    fi
+
+    JSON_REPORT="{\"channel\":\"${REPORT_URL_CHANNEL_VAR}\", \"username\":\"${PROJECT_NAME_VAR}\", \"attachments\":[{\"color\":\"${REPORT_COLOR}\" , \"text\":\"${DEPLOY_STATUS}\nBuild: ${CIRCLE_SHA1_VAR}\" }]}";
+    REPORT_ANSWER=`curl -X POST -H 'Content-type: application/json' --data "$JSON_REPORT" "$REPORT_URL_VAR"`;
+    exit;
+  else
+    echo "ERROR: needs project_name, report url, report channel, commit hash - $0 -h for usage";
+  fi;
+  exit;
+}
+
 
 read_args "$@";
 
